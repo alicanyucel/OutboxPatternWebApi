fd4afe6053fff21f5d22a7cfecf1246e2d291389
Ali Can Yücel (The Coderman) <yucelalican@hotmail.com>
2025-01-27 17:17:55 +0300

    çay

---
+++ OutboxPatternWebApi/Controllers/OrdersController.cs
@@ -0,0 +1,10 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+
+namespace OutboxPatternWebApi.Controllers;
+
+[Route("api/[controller]/[action]")]
+[ApiController]
+public class OrdersController : ControllerBase
+{
+}
